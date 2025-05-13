a2b00a216655920783a173aad35f1deb5a56df0d
MuhammadAbuBakarr <bakarkrl29@gmail.com>
2025-05-13 19:33:51 +0500

    lOCAL

--- src/api/useAxios.js
+++ src/api/useAxios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const SERVER_URL = "https://polling-backend-eight.vercel.app";
+const SERVER_URL = "http://localhost:5000";
 const useAxios = () => {
   const user = JSON.parse(localStorage.getItem("user"));
   const token = user?.token;
